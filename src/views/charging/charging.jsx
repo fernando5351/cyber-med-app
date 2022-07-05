ffb1fa2bc6ebcfe7267f82cd44ee91cb86cd16c9
Akira Pocasangre <6373543@clases.edu.sv>
2022-07-04 20:37:02 -0600

    Desarrollo de la Pantalla de carga COMPLETADO

---
+++ src/views/charging/charging.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { StyleSheet, View, Image } from 'react-native';
+import ImagenCarga from '../../../assets/images/cibermed.png';
+
+const PantallaCarga = () => {
+  return (
+    <View style={styles.containerCarga}>
+    <Image style={styles.ImageCarga} source={ImagenCarga}/>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  containerCarga: {
+    flex: 1,
+    backgroundColor: '#8DCFEC',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  ImageCarga:{
+    width: 243,
+    height: 243,
+    
+  },
+});
+export default PantallaCarga;
