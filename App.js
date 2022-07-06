09e88c019a6e45f8c7d22e66eee3cb3e5f4b32a2
Akira Pocasangre <6373543@clases.edu.sv>
2022-07-06 14:28:23 -0600

    Pantalla Opciones (Registrarse o Iniciar Sesion) COMPLETADA

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View} from 'react-native';
 import PantallaCarga from './src/views/charging/charging';
+import OpcionesLogin from './src/views/login/opcions';
+import ProfileE from './src/views/profile/ProfileE';
+
 
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <PantallaCarga/>
+      <OpcionesLogin/>
       <StatusBar style="auto" />
     </View>
   );
