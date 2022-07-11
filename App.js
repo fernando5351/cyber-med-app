b4dbe2c862ea4f1e8556ec3faf6a6fd0b9a35500
Diego_503jr <diegocarias503@gmail.com>
2022-07-10 22:42:36 -0600

    React Navigation implementada para navegar entre las screens

--- App.js
+++ App.js
@@ -1,21 +1,31 @@
-import react from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { StyleSheet, View } from "react-native";
 import Constants from "expo-constants";
-/* import PantallaCarga from './src/views/charging/charging'; */
+import PantallaCarga from "./src/views/charging/charging";
 import Profile from "./src/views/profile/Profile";
 import ProfileE from "./src/views/profile/ProfileE";
 
+const Stack = createNativeStackNavigator();
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      {/* <PantallaCarga/> */}
-      <ProfileE />
-    </View>
+    <NavigationContainer>
+      <View style={styles.containerMainApp}>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="ProfileEdit"
+            component={Profile}
+            options={{ headerShown: false }}
+          />
+        </Stack.Navigator>
+      </View>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  containerMainApp: {
     flex: 1,
     marginTop: Constants.statusBarHeight,
   },
