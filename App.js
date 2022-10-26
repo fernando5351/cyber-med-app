3245ec69e06451eded0847780831718dfbec3af6
fernando5351 <isaacfernando5351@gmail.com>
2022-10-25 19:39:39 -0600

    descargando modulo de paypal

--- App.js
+++ App.js
@@ -3,15 +3,12 @@ import "react-native-gesture-handler";
 //Gestor de la App
 import { AppNav } from "./src/routes/AppNav";
 //Providers para pasarela de pago y de usuario
-import { StripeProvider } from "@stripe/stripe-react-native";
 import { AuthProvider } from "./src/context/AuthUser";
 
 export default function App() {
   return (
     <AuthProvider>
-      <StripeProvider publishableKey="pk_test_51LrX5lL1ZhEreWQqCU8x5ZBYIkynMrMc90JWPt7ky6JVFneuF5JCw8ckJkgpjkNRPJf8FdlVMjSkCQtQHxFzIeZj00D7Dm9grW">
         <AppNav />
-      </StripeProvider>
     </AuthProvider>
   );
 }
